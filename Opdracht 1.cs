6ba5de72e62f34e66f872bea0644ae481f86de54
JasperTempelman11 <55128845+JasperTempelman1@users.noreply.github.com>
2019-11-13 09:37:26 +0100

    Add files via upload

---
+++ Opdracht 1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Opdracht_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welkom Bij de BMI berekening");
+            Console.WriteLine("Geprogameerd door Jasper Tempelman");
+
+             Console.Write("Gewicht in kilogram: ");
+            int kg;
+            kg = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Hoogte in centimers: ");
+            int m;
+            m = Convert.ToInt32(Console.ReadLine());
+            
+        
+           double BMI = kg / ( ( m / 100.0 ) * ( m / 100.0 ) );
+            Console.WriteLine("Je BMI is '{0}'", BMI);
+
+        }
+    }
+}
---
+++ Opdracht 3.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Opdracht_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        string[] taal = { "De huidige maand is April.", "Der aktuelle Monat ist April.", "The current month is April", "Le mois en cours est avril.", "El mes actual es abril.", " The current month is April." };
+
+        Console.WriteLine("1 = Nederlands\n2 = Duits\n3 = Engels\n4 = Frans\n5 = Spaans\n6 = mijn taal ");
+
+        Console.WriteLine("type num: ");
+        int num = Convert.ToInt32( Console.ReadLine());
+        Console.WriteLine(taal[num-=1]);
+        }
+    }
+}
